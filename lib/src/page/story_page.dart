bb4783ba3bff51984145a8f7607494afc1d6bf2b
jiang qiang <jiangtingqiang@gmail.com>
2020-12-15 13:35:28 +0800

    [feature] 添加文章详情滚动监听

--- lib/src/page/story_page.dart
+++ lib/src/page/story_page.dart
@@ -7,39 +7,60 @@ import 'package:Cruise/src/models/Item.dart';
 import 'package:Cruise/src/common/Repo.dart';
 
 class StoryPage extends HookWidget {
-  const StoryPage({
+  StoryPage({
     Key key,
     @required this.item,
   }) : super(key: key);
 
   final Item item;
+  final bool toTop = false;
+  final String percent = "0%";
+
+  final ScrollController scrollController = new ScrollController();
+
+  void initState() {
+    scrollController.addListener(() {
+      //打印监听位置
+      print(scrollController.offset);
+      if (scrollController.offset < 1000 && toTop) {
+      } else if (scrollController.offset >= 1000 && toTop == false) {}
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    var percent = useState<String>("0");
+    useEffect(() => initState, const []);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Cruise'),
-        actions: [
-          if (item.parent != null)
-            IconButton(
-              icon: Icon(Feather.corner_left_up),
-              onPressed: () async {
-                Item parent = await Repo.fetchArticleItem(item.parent);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => StoryPage(item: parent)),
-                );
-              },
-            ),
-        ],
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(child: StoryInformation(item: item)),
-          CommentList(item: item)
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Cruise'),
+          actions: [
+            if (item.parent != null)
+              IconButton(
+                icon: Icon(Feather.corner_left_up),
+                onPressed: () async {
+                  Item parent = await Repo.fetchArticleItem(item.parent);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StoryPage(item: parent)),
+                  );
+                },
+              ),
+          ],
+        ),
+        body: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification sn) {
+            double progress = sn.metrics.pixels / sn.metrics.maxScrollExtent;
+            percent.value = "${(progress * 100).toInt()}%";
+          },
+          child: CustomScrollView(
+            slivers: [
+              SliverToBoxAdapter(child: StoryInformation(item: item)),
+              CommentList(item: item)
+            ],
+          ),
+        ));
   }
 }
