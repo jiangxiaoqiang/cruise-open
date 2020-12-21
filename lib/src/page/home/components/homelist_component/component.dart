18daa5931868be45407d27c077d1725031ff7008
jiang qiang <jiangtingqiang@gmail.com>
2020-12-21 21:02:33 +0800

    [feature] 添加组件

--- lib/src/page/home/components/homelist_component/component.dart
+++ lib/src/page/home/components/homelist_component/component.dart
@@ -1,22 +1,17 @@
 import 'package:fish_redux/fish_redux.dart';
 
-import 'effect.dart';
 import 'reducer.dart';
 import 'state.dart';
 import 'view.dart';
 
-class HomeListPage extends Page<HomeListState, Map<String, dynamic>> {
-  HomeListPage()
+class HomeListComponent extends Component<HomeListState> {
+  HomeListComponent()
       : super(
-            initState: initState,
-            effect: buildEffect(),
             reducer: buildReducer(),
             view: buildView,
             dependencies: Dependencies<HomeListState>(
                 adapter: null,
                 slots: <String, Dependent<HomeListState>>{
-                }),
-            middleware: <Middleware<HomeListState>>[
-            ],);
+                }),);
 
 }
