fddd0a982c4099fc37d057d986308699defe29e6
jiangxiaoqiang <jiangtingqiang@gmail.com>
2020-12-23 09:51:36 -0500

    fish redux重写个人中心
    fish redux重写个人中心

--- lib/src/page/user/settings/cruisesetting/component.dart
+++ lib/src/page/user/settings/cruisesetting/component.dart
@@ -1,22 +1,17 @@
 import 'package:fish_redux/fish_redux.dart';
 
-import 'effect.dart';
 import 'reducer.dart';
 import 'state.dart';
 import 'view.dart';
 
-class CruiseSettingPage extends Page<CruiseSettingState, Map<String, dynamic>> {
-  CruiseSettingPage()
+class CruiseSettingComponent extends Component<CruiseSettingState> {
+  CruiseSettingComponent()
       : super(
-            initState: initState,
-            effect: buildEffect(),
             reducer: buildReducer(),
             view: buildView,
             dependencies: Dependencies<CruiseSettingState>(
                 adapter: null,
                 slots: <String, Dependent<CruiseSettingState>>{
-                }),
-            middleware: <Middleware<CruiseSettingState>>[
-            ],);
+                }),);
 
 }
