9067c262eabc794c0126b32a36863586ba158557
jiang qiang <jiangtingqiang@gmail.com>
2020-12-23 21:42:53 +0800

    我的关注使用fish redux重写
    我的关注使用fish redux重写

--- lib/src/page/home/components/homelistdefault_component/state.dart
+++ lib/src/page/home/components/homelistdefault_component/state.dart
@@ -1,20 +1,23 @@
+import 'package:Cruise/src/models/Item.dart';
 import 'package:Cruise/src/page/home/components/homelist_component/state.dart';
 import 'package:fish_redux/fish_redux.dart';
 
 class HomeListDefaultState implements Cloneable<HomeListDefaultState> {
-
-
+  StoriesType currentStoriesType;
 
   @override
   HomeListDefaultState clone() {
-    return HomeListDefaultState();
+    return HomeListDefaultState()
+    ..currentStoriesType = this.currentStoriesType;
   }
 }
 
-class HomeListDefaultConnector extends ConnOp<HomeListState, HomeListDefaultState> {
+class HomeListDefaultConnector
+    extends ConnOp<HomeListState, HomeListDefaultState> {
   @override
   HomeListDefaultState get(HomeListState state) {
     HomeListDefaultState substate = state.homeListDefaultState.clone();
+    substate.currentStoriesType = state.currentStoriesType;
     return substate;
   }
 }
