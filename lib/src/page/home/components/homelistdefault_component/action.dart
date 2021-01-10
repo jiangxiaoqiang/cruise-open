4afac16f3d6851d5c7b5e10e5e45d17eda8e4475
jiang qiang <jiangtingqiang@gmail.com>
2021-01-10 14:13:01 +0800

    [feature] 新增上拉刷新特性

--- lib/src/page/home/components/homelistdefault_component/action.dart
+++ lib/src/page/home/components/homelistdefault_component/action.dart
@@ -4,21 +4,28 @@ import 'package:fish_redux/fish_redux.dart';
 
 //TODO replace with your own action
 enum HomeListDefaultAction {
-  action,
   loading_more_articles,
   loading_more_articles_update,
   fetch_articleIds,
+  fetch_newest_articles,
+  fetch_newest_articles_update,
   set_articleIds,
 }
 
 class HomeListDefaultActionCreator {
-  static Action onAction() {
-    return const Action(HomeListDefaultAction.action);
-  }
 
   static Action onLoadingMoreArticles(ArticleRequest articleRequest) {
     return Action(HomeListDefaultAction.loading_more_articles,
         payload: articleRequest);
+  }
+
+  static Action onFetchNewestArticles(ArticleRequest articleRequest) {
+    return Action(HomeListDefaultAction.fetch_newest_articles, payload: articleRequest);
+  }
+
+  static Action onFetchNewestArticlesUpdate(List<Item> articles) {
+    return Action(HomeListDefaultAction.fetch_newest_articles_update,
+        payload: articles);
   }
 
   static Action onLoadingMoreArticlesUpdate(List<Item> articles) {
