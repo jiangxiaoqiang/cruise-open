d4b5cd30e552112bdd1a009178ffaf207ac48e7d
jiang qiang <jiangtingqiang@gmail.com>
2021-01-15 00:19:29 +0800

    [feature] 调整首屏加载文章数量，调整预加载时机

--- lib/src/page/home/view.dart
+++ lib/src/page/home/view.dart
@@ -4,45 +4,49 @@ import 'package:Cruise/src/page/home/action.dart';
 import 'package:Cruise/src/page/home/home_model.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
+
 import 'state.dart';
 
 Widget buildView(HomeState state, Dispatch dispatch, ViewService viewService) {
+  var context = viewService.context;
 
   void _onItemTapped(int index) {
     var homeModel = new HomeModel();
-    if(index == MenuType.my.value) {
+    if (index == MenuType.my.value) {
       homeModel.selectIndex = index;
       homeModel.storiesType = StoriesType.profile;
     }
-    if(index == MenuType.home.value){
+    if (index == MenuType.home.value) {
       homeModel.selectIndex = index;
       homeModel.storiesType = StoriesType.topStories;
     }
-    if(index == MenuType.channels.value){
+    if (index == MenuType.channels.value) {
       homeModel.selectIndex = index;
       homeModel.storiesType = StoriesType.channels;
     }
-    if(index == MenuType.follow.value){
+    if (index == MenuType.follow.value) {
       homeModel.selectIndex = index;
       homeModel.storiesType = StoriesType.subStories;
     }
     dispatch(HomeActionCreator.onSwitchNav(homeModel));
   }
 
+  //CruiseLocalizations cruiseLocalizations = CruiseLocalizations.of(context);
+  var home = null; //cruiseLocalizations.cruiseNavigatorHome;
+
   return Scaffold(
     body: viewService.buildComponent("homelist"),
     bottomNavigationBar: BottomNavigationBar(
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('首页')),
-        BottomNavigationBarItem(icon: Icon(Icons.follow_the_signs), title: Text('关注')),
-        BottomNavigationBarItem(icon: Icon(Icons.rss_feed), title: Text('频道')),
-        BottomNavigationBarItem(icon: Icon(Icons.school),title: Text('我的')),
-      ],
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: home == null ? "home" : home),
+          BottomNavigationBarItem(icon: Icon(Icons.follow_the_signs), label: '关注'),
+          BottomNavigationBarItem(icon: Icon(Icons.rss_feed), label: '频道'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: '我的'),
+        ],
         currentIndex: state.selectIndex,
         fixedColor: Colors.blue,
         onTap: _onItemTapped,
         unselectedItemColor: Color(0xff666666),
-        type: BottomNavigationBarType.fixed
-    ),
+        type: BottomNavigationBarType.fixed),
   );
 }
