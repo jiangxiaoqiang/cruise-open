ea4b4a3e363e1d6275968df5ecb61421dae0d661
jiang qiang <jiangtingqiang@gmail.com>
2021-01-17 21:23:01 +0800

    [feature] add intl demo

--- lib/src/widgets/CruiseApp.dart
+++ lib/src/widgets/CruiseApp.dart
@@ -6,6 +6,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:Cruise/src/common/theme.dart';
 import 'package:Cruise/src/common/view_manager.dart';
 import 'package:fish_redux/src/redux_component/page.dart' as fishPage;
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CruiseApp extends HookWidget {
   CruiseApp({@required this.theme, @required this.view});
@@ -28,6 +30,21 @@ class CruiseApp extends HookWidget {
     return MaterialApp(
         title: 'Cruise',
         theme: currentTheme,
+        localizationsDelegates: [
+          // ... app-specific localization delegate[s] here
+          // TODO: uncomment the line below after codegen
+          // AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalizations.delegate
+        ],
+        supportedLocales: [
+          const Locale('en', ''), // English, no country code
+          const Locale('ar', ''), // Arabic, no country code
+          const Locale.fromSubtags(languageCode: 'zh'), // Chinese *See Advanced Locales below*
+          // ... other locales the app supports
+        ],
         //home: HomeNew(),
         home: routes.buildPage('home', null),
         onGenerateRoute: (RouteSettings settings) {
