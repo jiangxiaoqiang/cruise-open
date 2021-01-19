eb1a782946c2d694b7a9eeea3ed04ebb4210101b
jiang qiang <jiangtingqiang@gmail.com>
2021-01-19 09:51:12 +0800

    [feature] 新增频道自动加载更多特性

--- lib/src/page/channel/channellistdefault_component/effect.dart
+++ lib/src/page/channel/channellistdefault_component/effect.dart
@@ -1,25 +1,43 @@
 import 'package:Cruise/src/common/Repo.dart';
+import 'package:Cruise/src/models/Channel.dart';
 import 'package:Cruise/src/models/Item.dart';
 import 'package:Cruise/src/models/request/article/article_request.dart';
 import 'package:fish_redux/fish_redux.dart';
+
 import 'action.dart';
 import 'state.dart';
 
 Effect<ChannelListDefaultState> buildEffect() {
   return combineEffects(<Object, Effect<ChannelListDefaultState>>{
     Lifecycle.initState: _onInit,
+    ChannelListDefaultAction.loading_more_channels: _onLoadingMoreChannels,
   });
 }
 
 Future _onInit(Action action, Context<ChannelListDefaultState> ctx) async {
-  ArticleRequest articleRequest = new ArticleRequest(
-      pageSize: 15,
-      pageNum: 1,
-      storiesType: StoriesType.channels
-  );
-  List<int> ids = await Repo.getArticleIds(articleRequest);
+  ArticleRequest articleRequest = new ArticleRequest(pageSize: 15, pageNum: 1, storiesType: StoriesType.channels);
+  List<int> ids = await Repo.getElementIds(articleRequest);
   if (ids != null) {
     ctx.dispatch(ChannelListDefaultActionCreator.onLoadingChannels(ids));
   }
 }
 
+Future _onLoadingMoreChannels(Action action, Context<ChannelListDefaultState> ctx) async {
+  ArticleRequest articleRequest = (action.payload as ArticleRequest);
+  articleRequest.pageNum = articleRequest.pageNum + 1;
+  ChannelListDefaultState homeListDefaultState = ctx.state;
+  if (homeListDefaultState.articleRequest.offset != null && homeListDefaultState.articleRequest.offset > 0) {
+    articleRequest.offset = homeListDefaultState.articleRequest.offset;
+  }
+  List<int> ids = await Repo.getElementIds(articleRequest);
+  List<Channel> channels = [];
+  if (ids != null) {
+    for (int id in ids) {
+      Channel channel = await Repo.fetchChannelItem(id);
+      if (channel != null) {
+        channels.add(channel);
+      }
+    }
+    ctx.dispatch(ChannelListDefaultActionCreator.onLoadingMoreChannelsUpdate(channels));
+  }
+}
