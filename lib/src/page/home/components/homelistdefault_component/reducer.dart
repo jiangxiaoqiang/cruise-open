17d4f580dd27515bd8dccbebf496ce2722625821
jiang qiang <jiangtingqiang@gmail.com>
2021-01-20 22:42:12 +0800

    [feature] 添加首页菜单点击回到顶部特性

--- lib/src/page/home/components/homelistdefault_component/reducer.dart
+++ lib/src/page/home/components/homelistdefault_component/reducer.dart
@@ -9,43 +9,43 @@ import 'state.dart';
 Reducer<HomeListDefaultState> buildReducer() {
   return asReducer(
     <Object, Reducer<HomeListDefaultState>>{
-      HomeListDefaultAction.loading_more_articles_update:
-          _onLoadingMoreArticlesUpdate,
-      HomeListDefaultAction.fetch_newest_articles_update:
-          _onFetchNewestArticlesUpdate,
+      HomeListDefaultAction.loading_more_articles_update: _onLoadingMoreArticlesUpdate,
+      HomeListDefaultAction.fetch_newest_articles_update: _onFetchNewestArticlesUpdate,
       HomeListDefaultAction.set_articleIds: _onSetArticleIds,
-      HomeListDefaultAction.update_article_loading_status:
-          _onUpdateArticleLoadingStatus,
+      HomeListDefaultAction.update_article_loading_status: _onUpdateArticleLoadingStatus,
+      HomeListDefaultAction.resume_scroll_top: _onResumeScrollTop,
     },
   );
 }
 
-HomeListDefaultState _onUpdateArticleLoadingStatus(
-    HomeListDefaultState state, Action action) {
+HomeListDefaultState _onResumeScrollTop(HomeListDefaultState state, Action action) {
+  final HomeListDefaultState newState = state.clone();
+  newState.isScrollTop = false;
+  return newState;
+}
+
+HomeListDefaultState _onUpdateArticleLoadingStatus(HomeListDefaultState state, Action action) {
   final HomeListDefaultState newState = state.clone();
   ArticleLoadingStatus loadingStatus = (action.payload as ArticleLoadingStatus);
   newState.articleLoadingStatus = loadingStatus;
   return newState;
 }
 
-HomeListDefaultState _onLoadingMoreArticlesUpdate(
-    HomeListDefaultState state, Action action) {
+HomeListDefaultState _onLoadingMoreArticlesUpdate(HomeListDefaultState state, Action action) {
   final HomeListDefaultState newState = state.clone();
   List<Item> articles = (action.payload as List<Item>);
   newState.articleListState.articles.addAll(articles);
   return newState;
 }
 
-HomeListDefaultState _onFetchNewestArticlesUpdate(
-    HomeListDefaultState state, Action action) {
+HomeListDefaultState _onFetchNewestArticlesUpdate(HomeListDefaultState state, Action action) {
   final HomeListDefaultState newState = state.clone();
   List<Item> articles = (action.payload as List<Item>);
   newState.articleListState.articles = articles;
   return newState;
 }
 
-HomeListDefaultState _onSetArticleIds(
-    HomeListDefaultState state, Action action) {
+HomeListDefaultState _onSetArticleIds(HomeListDefaultState state, Action action) {
   final HomeListDefaultState newState = state.clone();
   ArticlePayload payload = (action.payload as ArticlePayload);
   newState.articleListState.articleIds = payload.articleIds;
--- lib/src/page/home/state.dart
+++ lib/src/page/home/state.dart
@@ -3,19 +3,18 @@ import 'package:Cruise/src/page/channel/channellistdefault_component/state.dart'
 import 'package:Cruise/src/page/home/components/homelist_component/state.dart';
 import 'package:Cruise/src/page/home/components/homelistdefault_component/state.dart';
 import 'package:fish_redux/fish_redux.dart';
+import 'package:flutter/cupertino.dart';
 
 class HomeState implements Cloneable<HomeState> {
   int selectIndex = 0;
   StoriesType storiesType = StoriesType.topStories;
   HomeListState homeListState = new HomeListState();
-  HomeListDefaultState homeListDefaultState = new HomeListDefaultState();
   ChannelListDefaultState channelListDefaultState = new ChannelListDefaultState();
 
   @override
   HomeState clone() {
     return HomeState()
       ..selectIndex = this.selectIndex
-      ..homeListDefaultState = this.homeListDefaultState
       ..channelListDefaultState = this.channelListDefaultState
       ..homeListState = this.homeListState
       ..storiesType = this.storiesType;
@@ -27,6 +26,5 @@ HomeState initState(Map<String, dynamic> args) {
     ..selectIndex = 0
     ..homeListState = HomeListState()
     ..channelListDefaultState = new ChannelListDefaultState()
-    ..homeListDefaultState = new HomeListDefaultState()
     ..storiesType = StoriesType.topStories;
 }
