a7581509cf101b48040d2472d009de02a941237f
jiang qiang <jiangtingqiang@gmail.com>
2021-02-07 18:23:29 +0800

    [feature] 添加推荐获取接口

--- lib/src/common/utils/common_utils.dart
+++ lib/src/common/utils/common_utils.dart
@@ -1,6 +1,31 @@
 
 
+import 'package:Cruise/src/component/channel_compact_tile.dart';
+import 'package:Cruise/src/component/channel_item_card.dart';
+import 'package:Cruise/src/component/channel_item_tile.dart';
+import 'package:Cruise/src/models/Channel.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../view_manager.dart';
+
 class CommonUtils{
+
+  static Widget getChannelViewType(ViewType type, Channel item) {
+    switch (type) {
+      case ViewType.compactTile:
+        return ChannelCompactTile(item: item);
+        break;
+      case ViewType.itemCard:
+        return ChannelItemCard(item: item);
+        break;
+      case ViewType.itemTile:
+        return ChannelItemTile(item: item);
+        break;
+      default:
+        return ChannelItemCard(item: item);
+        break;
+    }
+  }
 
 }
 
