1208c448a4ec7e1222930264dc49cb268fd51c63
jiang qiang <jiangtingqiang@gmail.com>
2021-02-11 13:31:06 +0800

    [feature] 添加意见反馈页面

--- lib/src/page/user/feedback/action.dart
+++ lib/src/page/user/feedback/action.dart
@@ -1,10 +1,14 @@
 import 'package:fish_redux/fish_redux.dart';
 
 //TODO replace with your own action
-enum FeedbackAction { action }
+enum FeedbackAction { action, submit }
 
 class FeedbackActionCreator {
   static Action onAction() {
     return const Action(FeedbackAction.action);
+  }
+
+  static Action onSubmit(String feedback) {
+    return Action(FeedbackAction.submit, payload: feedback);
   }
 }
