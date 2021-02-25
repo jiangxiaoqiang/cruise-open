9e415dab2eed3e1cd21872dff6ecad855f2bde45
jiang qiang <jiangtingqiang@gmail.com>
2021-02-25 23:03:22 +0800

    [feature] 问题反馈

--- lib/src/page/user/feedback/view.dart
+++ lib/src/page/user/feedback/view.dart
@@ -1,4 +1,4 @@
-import 'package:Cruise/src/common/channel_action.dart';
+import 'package:Cruise/src/common/feedback_rest_action.dart';
 import 'package:Cruise/src/common/net/rest/http_result.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
@@ -7,44 +7,42 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'state.dart';
 
 Widget buildView(FeedbackState state, Dispatch dispatch, ViewService viewService) {
-
   double screenWidth = MediaQuery.of(viewService.context).size.width;
   var submitting = false;
+  String feedbackContent;
 
-  void handleSubmitFeedback() async {
-      submitting = true;
-      HttpResult result = await ChannelAction.addChannel(
-        //url: url.value,
-      );
+  void handleSubmitFeedback(String feedback) async {
+    submitting = true;
+    HttpResult result = await FeedbackRestAction.submitFeedback(feedback);
 
-      if (result.result == Result.error) {
-        Fluttertoast.showToast(
-            msg: "意见提交失败，请检查地址是否正确",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.CENTER,
-            timeInSecForIosWeb: 1,
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0);
-      } else {
-        Fluttertoast.showToast(
-            msg: "提交成功",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.CENTER,
-            timeInSecForIosWeb: 1,
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0);
-      }
-      submitting = false;
+    if (result.result == Result.error) {
+      Fluttertoast.showToast(
+          msg: "意见提交失败，请检查地址是否正确",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    } else {
+      Fluttertoast.showToast(
+          msg: "提交成功",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+    submitting = false;
   }
-
 
   return Scaffold(
     appBar: AppBar(
       title: Text("意见建议"),
     ),
-    body: SafeArea(child: Form(
+    body: SafeArea(
+        child: Form(
       child: ListView(
         children: [
           Padding(
@@ -57,7 +55,7 @@ Widget buildView(FeedbackState state, Dispatch dispatch, ViewService viewService
               maxLines: null,
               autocorrect: false,
               onChanged: (value) {
-                //url.value = value;
+                feedbackContent = value;
               },
               obscureText: false,
               enableInteractiveSelection: true,
@@ -89,21 +87,21 @@ Widget buildView(FeedbackState state, Dispatch dispatch, ViewService viewService
                         height: 50.0,
                         child: Center(
                             child: RaisedButton(
-                              color: Theme.of(context).primaryColor,
-                              shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                              onPressed: () async {
-                                handleSubmitFeedback();
-                              },
-                              child: submitting
-                                  ? SizedBox(
-                                height: 15,
-                                width: 15,
-                                child: CircularProgressIndicator(
-                                  backgroundColor: Colors.white,
-                                ),
-                              )
-                                  : Text("提交"),
-                            )));
+                          color: Theme.of(context).primaryColor,
+                          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                          onPressed: () async {
+                            handleSubmitFeedback(feedbackContent);
+                          },
+                          child: submitting
+                              ? SizedBox(
+                                  height: 15,
+                                  width: 15,
+                                  child: CircularProgressIndicator(
+                                    backgroundColor: Colors.white,
+                                  ),
+                                )
+                              : Text("提交"),
+                        )));
                   },
                 ),
               ),
