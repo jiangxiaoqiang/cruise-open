9e146c0c3a3b1a9745a2b80ff4f9a8ce8a0d42b4
jiang qiang <jiangtingqiang@gmail.com>
2021-02-28 13:16:47 +0800

    [feature] 关闭调试输出

--- lib/src/page/home/view.dart
+++ lib/src/page/home/view.dart
@@ -5,7 +5,6 @@ import 'package:Cruise/src/page/home/home_model.dart';
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
 import 'state.dart';
 
 Widget buildView(HomeState state, Dispatch dispatch, ViewService viewService) {
