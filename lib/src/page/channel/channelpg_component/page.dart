6afe1c4644eabb6451e2ee71bd441c3b50a083df
jiang qiang <jiangtingqiang@gmail.com>
2021-03-05 09:42:58 +0800

    [fix] 订阅列表搜索无法打开详情页面修复

---
+++ lib/src/page/channel/channelpg_component/page.dart
@@ -0,0 +1,24 @@
+import 'package:Cruise/src/page/channel/channeldetail_component/component.dart';
+import 'package:Cruise/src/page/channel/channeldetail_component/state.dart';
+import 'package:fish_redux/fish_redux.dart';
+
+import 'reducer.dart';
+import 'state.dart';
+import 'view.dart';
+
+class ChannelpgPage extends Page<ChannelPgState, Map<String, dynamic>> {
+  ChannelpgPage()
+      : super(
+            initState: initState,
+            reducer: buildReducer(),
+            view: buildView,
+            dependencies: Dependencies<ChannelPgState>(
+                adapter: null,
+                slots: <String, Dependent<ChannelPgState>>{
+                  'channeldetail': ChannelDetailConnector() +
+                      ChannelDetailComponent()
+                }),
+            middleware: <Middleware<ChannelPgState>>[
+            ],);
+
+}
