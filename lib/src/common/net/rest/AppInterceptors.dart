6010f16d0c81703d7d8677d99bd3717a989e6e1d
jiang qiang <jiangtingqiang@gmail.com>
2021-03-18 14:37:02 +0800

    [feature] 调整token刷新逻辑

--- lib/src/common/net/rest/AppInterceptors.dart
+++ lib/src/common/net/rest/AppInterceptors.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
+
 import 'package:Cruise/src/common/auth.dart';
+import 'package:Cruise/src/common/net/rest/rest_clinet.dart';
 import 'package:Cruise/src/models/api/response_status.dart';
 import 'package:dio/dio.dart';
+
 import '../../global.dart';
 import 'http_result.dart';
 
 class AppInterceptors extends InterceptorsWrapper {
   @override
-  Future onRequest(RequestOptions options) async{
+  Future onRequest(RequestOptions options) async {
     if (!options.headers.containsKey("token")) {
       String token = await storage.read(key: "token");
       options.headers["token"] = token;
@@ -15,24 +18,28 @@ class AppInterceptors extends InterceptorsWrapper {
     }
     return super.onRequest(options);
   }
+
   @override
-  Future onResponse(Response response) async{
-    if(response.data["statusCode"] == ResponseStatus.NOT_LOGIN.statusCode){
-      AuthResult result = await Auth.login(
-        username:await storage.read(key:"username"),
-        password: await storage.read(key:"password"),
-      );
-
-      if (result.result == Result.error) {
-
-      }
-    }
+  Future onResponse(Response response) async {
     return super.onResponse(response);
   }
+
   @override
-  Future onError(DioError err) {
-    // print("ERROR[${err?.response?.statusCode}] => PATH: ${err?.request?.path}");
+  Future onError(DioError err) async {
+    if (err.response != null && err.response.data["statusCode"] == ResponseStatus.NOT_LOGIN.statusCode) {
+      Dio dio = RestClient.createDio();
+      dio.lock();
+      AuthResult result = await Auth.login(
+        username: await storage.read(key: "username"),
+        password: await storage.read(key: "password"),
+      );
+      if (result.result == Result.ok) {
+        // resend a request to fetch data
+        Dio req = RestClient.createDio();
+        req.request(err.request.path);
+      }
+      dio.unlock();
+    }
     return super.onError(err);
   }
-
 }
