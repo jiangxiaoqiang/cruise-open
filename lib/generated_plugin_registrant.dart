3bf2de70037b566ce11c3caddf8a33c2e65d1352
jiang qiang <jiangtingqiang@gmail.com>
2021-05-18 09:39:42 +0800

    [tweak] remove firebase

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -4,7 +4,6 @@
 
 // ignore_for_file: lines_longer_than_80_chars
 
-import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -15,7 +14,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  FirebaseCoreWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
