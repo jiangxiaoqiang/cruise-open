1735362b132934a2bd719b2dc0c50bbe1002e426
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-05-26 14:29:19 -0400

    [feature] 新增支付目录结构

---
+++ lib/src/common/pay/Payment.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+import 'package:flutter_inapp_purchase/flutter_inapp_purchase.dart';
+import 'package:flutter_inapp_purchase/modules.dart';
+
+class Payment{
+  
+
+}
+
+
+
+
+
