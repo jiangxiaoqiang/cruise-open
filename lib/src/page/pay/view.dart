cd8450baf09e9dc0783f33a8928e982802165008
jiang qiang <jiangtingqiang@gmail.com>
2021-05-27 14:11:42 +0800

    [feature] add purchase entry

--- lib/src/page/pay/view.dart
+++ lib/src/page/pay/view.dart
@@ -1,9 +1,17 @@
 import 'package:fish_redux/fish_redux.dart';
 import 'package:flutter/material.dart';
 
-import 'action.dart';
 import 'state.dart';
 
 Widget buildView(PayState state, Dispatch dispatch, ViewService viewService) {
-  return Container();
+  return Scaffold(
+      body: SafeArea(
+          child: Container(
+            child: ElevatedButton(
+              onPressed: () {  },
+              child: Text("订阅"),
+            ),
+          )
+      )
+  );
 }
--- lib/src/page/user/settings/cruisesetting/view.dart
+++ lib/src/page/user/settings/cruisesetting/view.dart
@@ -1,5 +1,6 @@
 import 'package:cruise/src/common/auth.dart';
 import 'package:cruise/src/common/nav/nav_util.dart';
+import 'package:cruise/src/page/pay/page.dart';
 import 'package:cruise/src/page/user/discover/page.dart';
 import 'package:cruise/src/page/user/fav/page.dart';
 import 'package:cruise/src/page/user/feedback/page.dart';
@@ -139,6 +140,25 @@ Widget buildView(CruiseSettingState state, Dispatch dispatch, ViewService viewSe
                               );
                             },
                           ))))*/
+              Padding(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                          color: Colors.white,
+                          child: ListTile(
+                            leading: Icon(EvaIcons.info),
+                            trailing: Icon(Icons.keyboard_arrow_right),
+                            title: Text("会员中心"),
+                            onTap: () async {
+                              var data = {'name': "payPage"};
+                              Widget payPage = PayPage().buildPage(data);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => payPage),
+                              );
+                            },
+                          )))),
             ],
           ),
         ),
