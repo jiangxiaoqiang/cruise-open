a6a71a18db9e4c16f45b2293b39f6c95db110301
jiangxiaoqiang <jiangtingqiang@gmail.com>
2021-06-28 11:31:44 -0400

    [feature] auto complete purchase

--- lib/src/page/pay/effect.dart
+++ lib/src/page/pay/effect.dart
@@ -55,6 +55,7 @@ void _listenToPurchaseUpdated(List<PurchaseDetails> purchaseDetailsList, Context
         RestLog.logger("PurchaseStatus error");
         _handleError(purchaseDetails.error!, ctx);
       } else if (purchaseDetails.status == PurchaseStatus.restored) {
+        await InAppPurchase.instance.completePurchase(purchaseDetails);
         verifyReceipt(purchaseDetails);
       }else if(purchaseDetails.status == PurchaseStatus.purchased ){
         RestLog.logger("purchaseDetails purchased:" + purchaseDetails.productID);
