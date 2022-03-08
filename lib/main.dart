b6f4de0e1c1ecf220991876319dfc890a4a9244b
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-03-08 09:43:01 +0800

    [feature] tweak

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:cruise/src/common/config/cruise_global_config.dart';
 import 'package:wheel/wheel.dart' show CommonUtils,ConfigType;
 
 void main() async {
-  CommonUtils.initialApp(ConfigType.DEV).whenComplete(() => {
-    CruiseGlobalConfig.loadApp(ConfigType.DEV)
+  CommonUtils.initialApp(ConfigType.PRO).whenComplete(() => {
+    CruiseGlobalConfig.loadApp(ConfigType.PRO)
   });
 }
