d208a539892e40da886b18cae454f640dd8ecb70
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-09-01 14:22:47 +0800

    feat: remove sub article detail unused files

--- lib/src/page/sub/subarticlepg_component/component.dart
+++ lib/src/page/sub/subarticlepg_component/component.dart
@@ -1,7 +1,3 @@
-import 'package:cruise/src/page/home/components/articledetail_component/component.dart';
-import 'package:cruise/src/page/home/components/articlepg_component/state.dart';
-import 'package:cruise/src/page/sub/subarticledetail_component/component.dart';
-import 'package:cruise/src/page/sub/subarticledetail_component/state.dart';
 import 'package:fish_redux/fish_redux.dart';
 
 import 'reducer.dart';
@@ -11,13 +7,8 @@ import 'view.dart';
 class SubArticlePgComponent extends Component<SubArticlePgState> {
   SubArticlePgComponent()
       : super(
-            reducer: buildReducer(),
-            view: buildView,
-            dependencies: Dependencies<SubArticlePgState>(
-                adapter: null,
-                slots: <String, Dependent<SubArticlePgState>>{
-                  'subarticledetail': SubArticleDetailConnector() +
-                      SubArticleDetailComponent()
-                }),);
-
+          reducer: buildReducer(),
+          view: buildView,
+          dependencies: Dependencies<SubArticlePgState>(adapter: null, slots: <String, Dependent<SubArticlePgState>>{}),
+        );
 }
